476c54a2f2477cb269dcc801dc900bf95eff2e13
RobertMNewton <robert.m.newton7@gmail.com>
2024-01-10 16:35:58 +1000

    Update transformer_memory.py

--- src/model/memory_models/transformer_memory.py
+++ src/model/memory_models/transformer_memory.py
@@ -16,7 +16,7 @@ def _transformer_decoder(feature_dim: int, attn_heads: int, depth: int = 12, mlp
 
     return TransformerDecoder(
         decoder_layer=decoder_layer,
-        num_layers=decoder_layer,
+        num_layers=depth,
         norm=norm,
     )
 
@@ -24,10 +24,13 @@ def _fc_layer(input_dims: int, output_dims: int, activation: Module = nn.ReLU) -
     return nn.Linear(input_dims, output_dims), activation()
 
 def _mlp_layer(input_dims: int, feature_dims: int, output_dims: int, depth: int = 2, activation: Module = nn.ReLU) -> Module:
-    return nn.Sequential(*([_fc_layer(input_dims, feature_dims, activation=activation)] \
-        + [_fc_layer(feature_dims, feature_dims, activation=activation) for _ in range(depth)] \
-        + [_fc_layer(feature_dims, output_dims, activation=activation)]))
+    mlp = []
+    mlp.extend(_fc_layer(input_dims, feature_dims, activation=activation))
+    for _ in range(depth):
+        mlp.extend(_fc_layer(feature_dims, feature_dims, activation=activation))
+    mlp.extend(_fc_layer(feature_dims, output_dims, activation=activation))
 
+    return nn.Sequential(*mlp)
 
 class TransformerMemory(Module):
     USER_WARNING = False
@@ -58,28 +61,31 @@ class TransformerMemory(Module):
         self.linear_projection_in = _mlp_layer(input_feature_dims, projection_feature_dims, memory_feature_dims)
         self.linear_projection_out = _mlp_layer(memory_feature_dims, projection_feature_dims, output_feature_dims)
 
-        self.initial_memory = nn.Parameter(torch.Rand((memory_size, memory_feature_dims)))
-        self.positional_embeddings = nn.Parameter(torch.Rand((memory_size + 1, memory_feature_dims)))
-        self.cls_token = nn.Parameter(torch.rand((1, memory_feature_dims)))
+        self.initial_memory = nn.Parameter(torch.rand((memory_size, memory_feature_dims)), requires_grad=True)
+        self.positional_embeddings = nn.Parameter(torch.rand((memory_size + 1, memory_feature_dims)), requires_grad=True)
+        self.cls_token = nn.Parameter(torch.rand((1, memory_feature_dims)), requires_grad=True)
 
-        self.memory = self.initial_memory
+        self.memory = torch.clone(self.initial_memory)
 
     def forward(self, feature_enc: Tensor) -> Tensor:
         if len(feature_enc.shape) == 3 and not TransformerMemory.USER_WARNING:
             print("User Warning: transformer memory has not had batches implemented! You may receive errors!")
 
-        memory_seq = torch.cat((self.cls_token, self.memory), dim=1) + self.positional_embeddings
+        memory_seq = torch.cat((self.cls_token, self.memory), dim=0) + self.positional_embeddings
         feature_enc = self.linear_projection_in(feature_enc)
 
         memory_enc = self.decoder(memory_seq, feature_enc)[0]
 
         # note that this is treating the memory tensor like a stack [most recent -> least recent]
-        self.memory[1:] = self.memory[:-1]
+        self.memory[1:] = torch.clone(self.memory[:-1])
         self.memory[0] = memory_enc
 
         memory_enc = self.linear_projection_out(memory_enc)
 
         return memory_enc
+    
+    def get_size(self) -> int:
+        return sum(p.numel() for p in self.parameters() if p.requires_grad)
 
 
 class Mini(TransformerMemory):
@@ -117,7 +123,7 @@ class Small(TransformerMemory):
             decoder_depth=6,
             attn_heads=8,
             mlp_dim=512,
-            decoder_norm=nn.LayerNorm(512),
+            decoder_norm=nn.LayerNorm(256),
         )
 
 class Medium(TransformerMemory):
@@ -153,7 +159,7 @@ class Base(TransformerMemory):
             memory_size=memory_size,
             memory_feature_dims=1024,
             decoder_depth=24,
-            attn_heads=24,
+            attn_heads=32,
             mlp_dim=2048,
             decoder_norm=nn.LayerNorm(1024),
         )
@@ -195,3 +201,4 @@ class XLarge(TransformerMemory):
             mlp_dim=4096,
             decoder_norm=nn.LayerNorm(2048),
         )
+        
