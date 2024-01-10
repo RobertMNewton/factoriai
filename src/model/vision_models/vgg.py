5a1b31c3e84ce981b78a01f4f1e3efc3f7e84acd
RobertMNewton <robert.m.newton7@gmail.com>
2024-01-10 17:28:42 +1000

    fix mlp

--- src/model/vision_models/vgg.py
+++ src/model/vision_models/vgg.py
@@ -20,10 +20,14 @@ def _vgg_layer(kernel_size: int, input_channels: int, feature_channels: int, dep
 def _fc_layer(input_dims: int, output_dims: int, activation: Module = nn.ReLU) -> Tuple[Module, Module]:
     return nn.Linear(input_dims, output_dims), activation()
 
-def _mlp_layer(input_dims: int, feature_dims: int, output_dims: int, depth: int = 2, activation: Module = nn.ReLU) -> List[Module]:
-    return [_fc_layer(input_dims, feature_dims, activation=activation)] \
-        + [_fc_layer(feature_dims, feature_dims, activation=activation) for _ in range(depth)] \
-        + [_fc_layer(feature_dims, output_dims, activation=activation)]
+def _mlp_layer(input_dims: int, feature_dims: int, output_dims: int, depth: int = 2, activation: Module = nn.ReLU) -> Module:
+    mlp = []
+    mlp.extend(_fc_layer(input_dims, feature_dims, activation=activation))
+    for _ in range(depth):
+        mlp.extend(_fc_layer(feature_dims, feature_dims, activation=activation))
+    mlp.extend(_fc_layer(feature_dims, output_dims, activation=activation))
+
+    return nn.Sequential(*mlp)
 
 def _compute_output_dims(*vgg_layers, input_dims: Tuple[int, int, int]) -> Tuple[int, int, int]:
     def out_dims(in_dims: int, padding: int, dilation: int, kernel_size: int, stride: int) -> int:
